4a827b12614e005d0461675b5111c61b13f6d0f8
jolojo500 <193266106+jolojo500@users.noreply.github.com>
2025-05-12 23:39:20 -0400

    booga booga2

---
+++ src/RabucksToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier:  MIT
+//todo redo License
+
+pragma solidity ^0.8.18; //double check
+
+contract RabucksToken {
+    function name() public pure returns (string memory){
+        return "Rabucks Token";
+    }
+
+    function totalSupply() public pure returns(uint256){
+        return //something ether
+    }
+}
